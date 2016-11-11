882b9184776963a9338714211455c375e80db546
Gadfly <gadflybsd@gmail.com>
2016-11-11 16:41:57 +0800

    调整数据库为mssql2008

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -35,13 +35,13 @@ return array(
     'DEFAULT_FILTER' => '', //全局过滤函数
 
     /* 数据库配置 */
-    'DB_TYPE'   => 'mysql', // 数据库类型
-    'DB_HOST'   => '10.211.55.5', // 服务器地址
+    'DB_TYPE'   => 'sqlsrv', // 数据库类型
+    'DB_HOST'   => '10.211.55.3', // 服务器地址
     'DB_NAME'   => 'water', // 数据库名
-    'DB_USER'   => 'root', // 用户名
-    'DB_PWD'    => 'root',  // 密码
-    'DB_PORT'   => '3306', // 端口
-    'DB_PREFIX' => 'onethink_', // 数据库表前缀
+    'DB_USER'   => 'sa', // 用户名
+    'DB_PWD'    => 'weA3#q7*',  // 密码
+    'DB_PORT'   => '1433', // 端口
+    'DB_PREFIX' => 'dbo.onethink_', // 数据库表前缀
 
     /* 文档模型配置 (文档模型核心配置，请勿更改) */
     'DOCUMENT_MODEL_TYPE' => array(2 => '主题', 1 => '目录', 3 => '段落'),
