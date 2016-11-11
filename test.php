e5df2c9ebd7e62f3327c1bddab91752835b668ba
Gadfly <gadflybsd@gmail.com>
2016-11-11 17:43:57 +0800

    no message

--- test.php
+++ test.php
@@ -5,4 +5,14 @@
  * Date: 2016/11/11
  * Time: 下午5:36
  */
-phpinfo();
+$username = "sa";
+$pwd = "weA3#q7*";
+try{
+	$dbh = new PDO('sqlsrv:host=10.211.55.3;dbname=water',$username,$pwd);
+	$sqlstri = "select * from dbo.onethink_action";
+	foreach ($dbh->query($sqlstri) as $row){
+		print_r($row);
+	}
+}catch (PDOException $e){
+	print "Error:".$e->getMessage()."<br>";
+}
