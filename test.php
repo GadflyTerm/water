665aa2ba05bdf280c43a50021bfa08784b1f7f66
Gadfly <gadflybsd@gmail.com>
2016-11-11 18:03:44 +0800

    no message

--- test.php
+++ test.php
@@ -5,10 +5,14 @@
  * Date: 2016/11/11
  * Time: 下午5:36
  */
+$hostname = "10.211.55.3";
+$dbname = "water";
 $username = "sa";
 $pwd = "weA3#q7*";
 try{
-	$dbh = new PDO('sqlsrv:host=10.211.55.3;dbname=water',$username,$pwd);
+	$dsn="sqlsrv:Server=$hostname;database=$dbname";
+	$dbh = new PDO ($dsn,$username,$pwd);
+	//$dbh = new PDO('sqlsrv:host=10.211.55.3;dbname=water',$username,$pwd);
 	$sqlstri = "select * from dbo.onethink_action";
 	foreach ($dbh->query($sqlstri) as $row){
 		print_r($row);
