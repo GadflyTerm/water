bfc525c5e886c4a4510cbb8d334d19ab7ecc6f0f
Gadfly <gadflybsd@gmail.com>
2016-11-14 16:51:27 +0800

    no message

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -41,7 +41,7 @@ return array(
     'DB_USER'   => 'sa', // 用户名
     'DB_PWD'    => 'weA3#q7*',  // 密码
     'DB_PORT'   => '1433', // 端口
-    'DB_PREFIX' => 'dbo.onethink_', // 数据库表前缀
+    'DB_PREFIX' => 'onethink_', // 数据库表前缀
 
     /* 文档模型配置 (文档模型核心配置，请勿更改) */
     'DOCUMENT_MODEL_TYPE' => array(2 => '主题', 1 => '目录', 3 => '段落'),
