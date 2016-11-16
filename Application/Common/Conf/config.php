ccae2c50bdf94944c376e9fa7b3a848f413acca5
Gadfly <gadflybsd@gmail.com>
2016-11-16 13:19:27 +0800

    no message

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -41,6 +41,7 @@ return array(
 	'DB_USER'   => 'PYGQ', 				// 用户名
 	'DB_PWD'    => 'lonhwin@2011sql',  	// 密码
 	'DB_PORT'   => '1433', 				// 端口
+	'DB_PREFIX' => 'dbo.', // 数据库表前缀
 
     /* 文档模型配置 (文档模型核心配置，请勿更改) */
     'DOCUMENT_MODEL_TYPE' => array(2 => '主题', 1 => '目录', 3 => '段落'),
