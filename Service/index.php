99981fd781aecc2faf0f93c961d8e1c42b209de8
Gadfly <gadflybsd@gmail.com>
2016-12-20 17:09:52 +0800

    新增雨量测站数据库触发器和水库测站数据库触发器

--- Service/index.php
+++ Service/index.php
@@ -29,7 +29,7 @@ define ( 'APP_PATH', './Application/' );
  * 缓存目录设置
  * 此目录必须可写，建议移动到非WEB目录
  */
-define ( 'RUNTIME_PATH', '../Runtime/water.com/' );
+define ( 'RUNTIME_PATH', '../../Runtime/water.com/' );
 
 /**
  * 引入核心入口
