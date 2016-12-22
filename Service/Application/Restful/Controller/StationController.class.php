11248089fd25e70a0d4b4becba83c5824bde94a4
Gadfly <gadflybsd@gmail.com>
2016-12-22 17:13:11 +0800

    将设置参数移至option.php

--- Service/Application/Restful/Controller/StationController.class.php
+++ Service/Application/Restful/Controller/StationController.class.php
@@ -55,35 +55,7 @@ class StationController extends Controller{
 					return array(
 						'type'	=> 'Success',
 						'msg'	=> '水质异常参照标准数据读取成功！',
-						'data'	=> array(
-							array('label' => '水温', 'field' => 'WT', 'min' => 2, 'value' => 25, 'prickle' => '℃', 'status' => 1),
-							array('label' => 'pH值', 'field' => 'PH', 'min' => 5.5, 'value' => 8.5, 'prickle' => '', 'status' => 1),
-							array('label' => '五日生化需氧量', 'field' => 'BOD5', 'min' => 15, 'value' => 40, 'prickle' => 'mg/L', 'status' => 1),
-							array('label' => '化学需氧量', 'field' => 'CODCR', 'min' => 60, 'value' => 100, 'prickle' => 'mg/L', 'status' => 1),
-							array('label' => '悬浮物', 'field' => 'SS', 'min' => 15, 'value' => 60, 'prickle' => 'mg/L', 'status' => 1),
-							array('label' => '阴离子表面活性剂', 'field' => 'LAS', 'min' => 0, 'value' => 5, 'prickle' => 'mg/L', 'status' => 1),
-							array('label' => '全盐量', 'field' => 'SO', 'min' => 0, 'value' => 1000, 'prickle' => 'mg/L', 'status' => 1),
-							array('label' => '氯化物', 'field' => 'CL', 'min' => 0, 'value' => 350, 'prickle' => 'mg/L', 'status' => 1),
-							array('label' => '硫化物', 'field' => 'S2', 'min' => 0, 'value' => 1, 'prickle' => 'mg/L', 'status' => 1),
-							array('label' => '总汞', 'field' => 'HG', 'min' => 0, 'value' => 0.001, 'prickle' => 'mg/L', 'status' => 1),
-							array('label' => '总镉', 'field' => 'CD', 'min' => 0, 'value' => 0.01, 'prickle' => 'mg/L', 'status' => 1),
-							array('label' => '总砷', 'field' => 'ARS', 'min' => 0, 'value' => 0.05, 'prickle' => 'mg/L', 'status' => 1),
-							array('label' => '六价铬', 'field' => 'CR6', 'min' => 0, 'value' => 0.1, 'prickle' => 'mg/L', 'status' => 1),
-							array('label' => '总铅', 'field' => 'PB', 'min' => 0, 'value' => 0.2, 'prickle' => 'mg/L', 'status' => 1),
-							array('label' => '总铜', 'field' => 'CU', 'min' => 0, 'value' => 1, 'prickle' => 'mg/L', 'status' => 1),
-							array('label' => '总锌', 'field' => 'ZN', 'min' => 0, 'value' => 2, 'prickle' => 'mg/L', 'status' => 1),
-							array('label' => '总硒', 'field' => 'SE', 'min' => 0, 'value' => 0.02, 'prickle' => 'mg/L', 'status' => 1),
-							array('label' => '氟化物', 'field' => 'F', 'min' => 0, 'value' => 2, 'prickle' => 'mg/L', 'status' => 1),
-							array('label' => '氰化物', 'field' => 'CN', 'min' => 0, 'value' => 0.5, 'prickle' => 'mg/L', 'status' => 1),
-							array('label' => '石油类', 'field' => 'OIL', 'min' => 0, 'value' => 1, 'prickle' => 'mg/L', 'status' => 1),
-							array('label' => '挥发酚', 'field' => 'VLPH', 'min' => 0, 'value' => 1, 'prickle' => 'mg/L', 'status' => 1),
-							array('label' => '苯', 'field' => 'BEN', 'min' => 0, 'value' => 2.5, 'prickle' => 'mg/L', 'status' => 1),
-							array('label' => '三氯乙醛', 'field' => 'SLYQ', 'min' => 0, 'value' => 0.5, 'prickle' => 'mg/L', 'status' => 1),
-							array('label' => '丙烯醛', 'field' => 'BXQ', 'min' => 0, 'value' => 0.5, 'prickle' => 'mg/L', 'status' => 1),
-							array('label' => '硼', 'field' => 'B', 'min' => 0, 'value' => 0.5, 'prickle' => 'mg/L', 'status' => 1),
-							array('label' => '粪大肠菌群数', 'field' => 'FCG', 'min' => 1000, 'value' => 2000, 'prickle' => '个/L', 'status' => 1),
-							array('label' => '蛔虫卵数', 'field' => 'AO', 'min' => 1, 'value' => 2, 'prickle' => '个/L', 'status' => 1),
-						)
+						'data'	=> C('OPT.WaterStandard')
 					);
 				}
 			default:
