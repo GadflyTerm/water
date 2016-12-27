9335d67bfb8c5a83816edf1da286bf85290a4fe3
Gadfly <gadflybsd@gmail.com>
2016-12-27 10:36:27 +0800

    no message

--- Client/javascript/controller/Station/WaterStationCtrl.js
+++ Client/javascript/controller/Station/WaterStationCtrl.js
@@ -145,7 +145,7 @@ define(function (require){
 				closeOnConfirm: false,
 				html: false
 			}, function(){
-				$scope.promise = xhr.service('post', {model: 'station', module: 'delWater', data: {STCD: stcd}}, function(resp){
+				$scope.promise = xhr.service('post', {model: 'station', module: 'delWater', data: JSON.stringify({STCD: stcd})}, function(resp){
 					$scope.list =resp.data;
 					if(resp.type == 'Success'){
 						swal("删除成功!", "您已经成功删除了一条水质采样测站数据", "success");
