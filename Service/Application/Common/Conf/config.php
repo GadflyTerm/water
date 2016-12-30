3068387ab8e2a66ee4efd8146776dba75b6590af
Gadfly <gadflybsd@gmail.com>
2016-12-30 18:59:43 +0800

    no message

--- Service/Application/Common/Conf/config.php
+++ Service/Application/Common/Conf/config.php
@@ -16,21 +16,21 @@ return array(
 	'TOKEN_TYPE'	=> 'md5',
 	'TOKEN_RESET'	=> false,
 	
-/*	'DB_TYPE'   		=> 'Sqlsrv', 			// 数据库类型
+	'DB_TYPE'   		=> 'Sqlsrv', 			// 数据库类型
 	'DB_HOST'   		=> '114.55.231.186', 	// 服务器地址
 	'DB_NAME'   		=> 'Hydrology_pygq', 	// 数据库名
 	'DB_USER'   		=> 'PYGQ', 				// 用户名
 	'DB_PWD'    		=> 'lonhwin@2011sql',  	// 密码
 	'DB_PORT'   		=> '1433', 				// 端口
 	'DB_PREFIX' 		=> 'dbo.', 				// 数据库表前缀
-	'DB_FIELDS_CACHE'	=> false,				// 关闭字段缓存*/
+	'DB_FIELDS_CACHE'	=> false,				// 关闭字段缓存
 
-	'DB_TYPE'   		=> 'Sqlsrv', 			// 数据库类型
+/*	'DB_TYPE'   		=> 'Sqlsrv', 			// 数据库类型
 	'DB_HOST'   		=> '127.0.0.1', 	// 服务器地址
 	'DB_NAME'   		=> 'master', 	// 数据库名
 	'DB_USER'   		=> 'sa', 				// 用户名
 	'DB_PWD'    		=> 'weA3#q7*',  	// 密码
 	'DB_PORT'   		=> '1433', 				// 端口
 	'DB_PREFIX' 		=> 'dbo.', 				// 数据库表前缀
-	'DB_FIELDS_CACHE'	=> false,				// 关闭字段缓存
+	'DB_FIELDS_CACHE'	=> false,				// 关闭字段缓存*/
 );
